6e4b909d1e41449caecc76f410816ebcbba07108
harveyzeng <ff6416@hotmail.com>
2016-03-05 02:12:20 +0800

    initial commit

--- testpattern_c/compile_script.sh
+++ testpattern_c/compile_script.sh
@@ -9,13 +9,16 @@ arm-none-eabi-gcc -mcpu=cortex-m4   -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -
 
 #link all object code to elf file
  
-arm-none-eabi-ld -T link.lds *.o -o test.elf -g
+arm-none-eabi-ld -T link.lds *.o -o nop.elf -g
 
 #get the binary code
 
-arm-none-eabi-objcopy -O binary test.elf test.bin
+arm-none-eabi-objcopy -O binary nop.elf nop.bin
 
 #get the symbol table and assembly code
 
-arm-none-eabi-objdump -xd test.elf > test.txt
+arm-none-eabi-objdump -xd nop.elf > nop.txt
 
+
+xxd -c 1 -ps nop.bin > nop.hex
+./hextohex
